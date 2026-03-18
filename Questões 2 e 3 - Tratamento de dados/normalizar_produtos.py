8e9abf1abd0900121ad90e98ac0b0131dae2e0a4
ricardojfrancisco <ricardojfrancisco82@gmail.com>
2026-03-18 19:13:03 -0300

    [Update]Registro de resolução - Questão 2
    Efetuada a limpeza e a normalização dos dados em "produtos_raaw.csv", gerado um novo arquivo .csv com os dados limpos (Produtos_clean.csv na pasta questões 2 e 3) além do script de limpeza e outros arquivos .csv auxiliares para relatório final

---
+++ Questões 2 e 3 - Tratamento de dados/normalizar_produtos.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 18 18:01:12 2026
+
+@author: ricardojfrancisco
+"""
+
+# Normalização da tabela produtos_raw: 
+    #Parte 1 — Padronize os nomes das categorias de produtos em: eletrônicos, propulsão e ancoragem.
+    #Parte 2 — Converta os valores para o tipo numérico.
+    #Parte 3 — Remova as duplicatas.
+
+
+import pandas as pd
+import numpy as np
+
+produtos_original = pd.read_csv(r'C:/Users/ricardojfrancisco/Desktop/Desafio Indicium/Arquivos originais/produtos_raw.csv')
+
+# Verifica a estrutiura básica e valores da tabela
+print(produtos_original.info())
+print(produtos_original.isnull().sum())
+
+#Categorizas precisa ser normalizado
+total_categorias = produtos_original['actual_category'].nunique() # temos 39 categorias
+    
+lista_categorias = (produtos_original['actual_category'].unique()) # retorna as ocorrÊncias
+
+lista_contagem = produtos_original['actual_category'].value_counts()
+lista_contagem.columns = ['categoria', 'quantidade']
+
+#Normalização das categorias - todas em caixa baixa
+produtos_original['actual_category'] = produtos_original['actual_category'].str.strip().str.lower()
+produtos_original['actual_category'].unique()
+
+#criando listas para uniformizar o cadastro
+
+produtos_original['actual_category'] = produtos_original['actual_category'].replace(['eletronicos', 'e l e t r ô n i c o s', 'eletrunicos', 'eletronicoz', 'eletrônicos', 'eletroniscos'], 'eletronicos') 
+produtos_original['actual_category'] = produtos_original['actual_category'].replace(['propulsao', 'propulção', 'prop', 'propulssão', 'p r o p u l s ã o', 'propução', 'propulsão', 'propulçao', 'propulsam'], 'propulsao')
+produtos_original['actual_category'] = produtos_original['actual_category'].replace(['ancoragem', 'encoragem','ancoraguem', 'ancorajm', 'a n c o r a g e m', 'ancorajem', 'encoragi', 'ancorajen', 'ancoragen'], 'ancoragem')
+produtos_original['actual_category'].unique()
+
+#São dois campos numéricos (price e code), porém price precisa ser convertido
+produtos_original['price'] = produtos_original['price'].str.replace('R$', '', regex=False).str.strip().astype(float)
+
+produtos_original['price'].head()
+
+#remover registros duplicados
+total_produtos = produtos_original['name'].nunique() # temos 150 produtos, mas 157 registros
+produtos_duplicados = produtos_original[produtos_original.duplicated(keep=False)] #rRepetidos
+produtos_limpa = produtos_original.drop_duplicates()
+
+#salvar as variáveis que precisarão ser demonstradas na analise, e fgerando a tabela de produtos limpa
+exports = {
+    'produtos_duplicados.csv': produtos_duplicados,
+    'produtos_clean.csv': produtos_limpa
+    }
+
+for nome_arquivo, data in exports.items():
+    data.to_csv(nome_arquivo, index=False, sep=';', encoding='utf-8-sig')
+    print(f"✅ Arquivo {nome_arquivo} exportado com sucesso!")
+
